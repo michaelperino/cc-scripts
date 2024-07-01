3782b58fcca38a88156f25776ff5c624663b8672
BuildTools <unconfigured@null.spigotmc.org>
2024-07-01 17:50:14 -0500

    add update script

---
+++ update.lua
@@ -0,0 +1,7 @@
+shell.run("delete floorer.lua")
+shell.run("delete item_provider.lua")
+shell.run("delete update.lua")
+
+shell.run("wget https://raw.githubusercontent.com/michaelperino/cc-scripts/master/update.lua")
+shell.run("wget https://raw.githubusercontent.com/michaelperino/cc-scripts/master/floorer.lua")
+shell.run("wget https://raw.githubusercontent.com/michaelperino/cc-scripts/master/item_provider.lua")
